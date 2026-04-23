49af5391472721b0a8d23780e0609f61d52a1ab5
Zeyn <zeynabi@gmx.de>
2026-04-23 23:21:57 +0200

    first commit

---
+++ src/main/java/de/htw_berlin/tripn/TripnApplication.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.tripn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw_berlin/tripn/TripnApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.tripn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
